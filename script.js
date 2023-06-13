59a0f4d63e853097173567fb8384b8f63e11cf55
Liv <livbendix@live.dk>
2023-06-13 17:04:36 +0200

    mobilsite

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var burgerMenu = document.getElementById("burger-menu");
+
+var overlay = document.getElementById("menu");
+
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
